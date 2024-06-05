4350ddd47786183c725c061fbbc8e0079e1f37d1
Fayas Muhammed <muhammedfayas796@gmail.com>
2024-06-05 15:36:39 +0530

    api for customers

---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from api import views
+
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+
+router.register("customers",views.CustomerViewsetView,basename="customers")
+
+
+urlpatterns=[
+
+    path("token/",ObtainAuthToken.as_view())
+
+]+router.urls
