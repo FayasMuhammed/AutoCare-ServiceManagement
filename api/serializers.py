11d8303bad60c615b2a14ab5307071fb1d3c55d9
Fayas Muhammed <muhammedfayas796@gmail.com>
2024-06-06 15:28:58 +0530

    update

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,22 @@
 
 from rest_framework import serializers
 
-from api.models import Customer
+from api.models import Customer,Work
+
+
+class WorkSerializer(serializers.ModelSerializer):
+
+    customer=serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+
+        model=Work
+
+        fields="__all__"
+
+        read_only_fields=["id","customer","created_date","update_date","is_active"]
+
+
 
 
 
@@ -10,10 +25,14 @@ class CustomerSerializer(serializers.ModelSerializer):
 
     technician=serializers.StringRelatedField(read_only=True)
 
+    work_count=serializers.CharField(read_only=True)
+
+    work_total=serializers.CharField(read_only=True)
+
     class Meta:
 
         model=Customer
 
         fields="__all__"
 
-        read_only_fields=["id","technician","status","created_date","updated_date","is_active"]
+        read_only_fields=["id","technician","status","created_date","update_date","is_active"]
--- api/views.py
+++ api/views.py
@@ -4,9 +4,11 @@ from rest_framework.viewsets import ModelViewSet
 
 from rest_framework.response import Response
 
+from rest_framework.decorators import action
+
 from rest_framework import authentication,permissions
 
-from api.serializers import CustomerSerializer
+from api.serializers import CustomerSerializer,WorkSerializer
 
 from api.models import Customer
 
@@ -25,5 +27,27 @@ class CustomerViewsetView(ModelViewSet):
     def perform_create(self, serializer):
         
         serializer.save(technician=self.request.user)
-        
+    
+    @action(methods=["post"],detail=True)
+    def add_work(self,request,*args,**kwargs):
+
+        # customer_instance=self.get_objects()
+
+        id=kwargs.get("pk")
+
+        customer_instance=Customer.objects.get(id=id)
+
+        serializer=WorkSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(customer=customer_instance)
+
+            return Response(data=serializer.data)
+
+        else:
+
+            return Response(data=serializer.errors)
+
+
 
